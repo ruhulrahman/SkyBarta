b0316c4eeb36c30030157f89e62b2111d4063d0a
ruhulrahman <ruhulrahman2233@gmail.com>
2020-10-10 23:21:43 +0600

    auth serviceprovider changes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,50 +2,23 @@
 
 namespace App\Providers;
 
-use App\Role;
 use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
+
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
     ];
 
     /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
+     * Register any authentication / authorization services.d
      */
     public function boot()
     {
         $this->registerPolicies();
-
-        // $super = '';
-        // $admin = "";
-        // $author ="";
-        // $user ="";
-
-        // $roles = Role::all();
-
-        // foreach ($roles as $role) {
-        //      if($role->name == "Super"){
-        //        $super = $role->id;
-        //     }else if($role->name == "Admin"){
-        //        $admin = $role->id;
-        //     }else if($role->name == "Admin"){
-        //        $author = $role->id;
-        //     }else if($role->name == "Admin"){
-        //        $user = $role->id;
-        //     }
-        // }
-
 
         Gate::define('isSuperAdmin', function ($user) {
             return $user->role_id === 1;
